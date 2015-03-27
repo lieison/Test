2eb2a28dcd27371f7352da6f540ef269c00e639d
rolando arriaza <rolignu90@gmail.com>
2015-03-26 20:10:00 -0600

    Test Modify

--- Acciones.js
+++ Acciones.js
@@ -3,13 +3,10 @@
 //ACA IRAN TODOS LOS PROCESOS Y ALGORITMOS EN EL CUAL LOGRARA EJECUTAR EL EDITAR 
 function Edit(id)
 {
-    
     //NOTA : ESTO ES EL EJEMPLO DE AJAX , NO QUIERA DECIR QUE ES TODO EL CODIGO QUE USARA
-    
     var params = {
           "id" : id
     };
-
     $.ajax({
       type: "POST",
       url: "Envio.php",
@@ -21,6 +18,16 @@ function Edit(id)
 
 //ACA IRA TODO LOS PROCESOS PARA LOGRAR EJECUTAR EL ELIMINAR
 function Delete(id){
+    var params = {
+          "id" : id
+    };
+    $.ajax({
+      type: "POST",
+      url: "Envio.php",
+      data: params,
+      success: function(value){
+             
+     }});
     //PUEDE USAR EL MISMO AJAX PARA ENVIAR LA PETICION
 }
 
